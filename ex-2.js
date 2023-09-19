4070ad8f10b6a57ad56cf639f5956d1732a28f8f
pranaiya suwanta <27mar03.jk@gmail.com>
2023-09-19 16:56:49 +0700

    finished exercise

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,11 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = True;
+let isWeekday = True;
+let hasBoughtProductFromITCategory = False;
+let hasAttendedDiscountEvent = True;
+let isPlatinum = False;
 
-let hasPromotion;
-
+let hasPromotion = (lastMonthPaidMoreThan4000) && (isWeekday) && (!hasBoughtProductFromITCategory) &&(!hasAttendedDiscountEvent) ||isPlatinum;
 console.log(hasPromotion);
